0ed300f8a6d1a2eb8305199704cc66850f96f3cf
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-05-18 19:46:44 +0530

    Copy to Clipboard

---
+++ Copy-To-Clipboard/script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("copy");
+const main_paragraph = document.getElementById("main_paragraph");
+const message = document.getElementById("message");
+function copyToClipboard(element) {
+    navigator.clipboard.writeText(element.textContent)
+        .then(() => {
+            message.textContent = "Copied to Clipboard";
+            message.classList.add("after-clicked");
+        }).catch((err) => {
+            console.log("Unable to copy the data");
+        }).finally(() => {
+            setTimeout(() => {
+                message.textContent = "";
+                message.classList.remove("after-clicked");
+            }, 2000);
+        });
+}
+button.addEventListener("click", () => copyToClipboard(main_paragraph));
