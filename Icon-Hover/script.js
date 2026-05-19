0f363b4074f1cf7beef5d106870bca8f90ace62d
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-05-19 11:37:14 +0530

    Mouse Leave Event

--- Icon-Hover/script.js
+++ Icon-Hover/script.js
@@ -1,5 +1,9 @@
 const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", (e) => {
+    cursor.style.display = "block";
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
+});
+document.addEventListener("mouseleave", (e) => {
+    cursor.style.display = "none";
 });
